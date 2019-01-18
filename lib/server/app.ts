90e248ec1b1ba76817d4f42ebb89ca06b5fd8e1f
Drew del Rosario <drewdelrosario@gmail.com>
2019-01-17 19:14:53 -0800

    Socket Server and express server are both connected

---
+++ lib/server/app.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import * as parser from 'body-parser';
+import { socketServer } from './socketServer';
+
+class App {
+  public app: express.Application;
+  private socketServer: socketServer;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.serveStatic();
+    this.socketServer = new socketServer();
+  }
+  //Apply middleware
+  private config(): void {
+    this.app.use(parser.json());
+    this.app.use(parser.urlencoded({ extended: true }));
+  }
+  //Serve static files
+  private serveStatic(): void {
+    this.app.use(express.static(__dirname + '/../static'));
+  }
+  //Router
+}
+
+export const app = new App().app;
---
+++ lib/server/socketServer.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import { createServer, Server } from 'http';
+import * as socketIo from 'socket.io';
+
+export class socketServer {
+  public app: express.Application;
+  private http: Server;
+  private io: socketIo.Server;
+  public readonly PORT: number = 3005;
+
+  constructor() {
+    this.app = express();
+    this.http = createServer(this.app);
+    this.io = socketIo(this.http);
+    this.serverListen();
+    this.listenForConnections();
+  }
+  private serverListen() {
+    this.http.listen(this.PORT, () => {
+      console.log('Socket Server is listening to port: ', this.PORT);
+    });
+  }
+  private listenForConnections() {
+    this.io.on('connection', (socket: socketIo.Socket) => {
+      console.log(socket.id + ' has connected');
+
+      socket.on('join', (username: String) => {
+        console.log('User joined with username: ', username);
+      });
+    });
+  }
+}
